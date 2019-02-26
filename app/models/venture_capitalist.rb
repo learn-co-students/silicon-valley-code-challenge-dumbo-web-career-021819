b35421357937725900d1fb5185752c0f0d834a3c
1-8192 <allegranzia@gmail.com>
2019-02-26 17:57:15 -0500

    Done.

--- app/models/venture_capitalist.rb
+++ app/models/venture_capitalist.rb
@@ -1,3 +1,57 @@
 class VentureCapitalist
 
+attr_reader :name
+attr_accessor :total_worth
+@@all = []
+
+def initialize (name, total_worth)
+  @name = name
+  @total_worth = total_worth
+  @@all << self
+end
+
+def offer_contract(startup, type, amount)
+  FundingRound.new(startup, self, type, amount)
+end
+
+def funding_rounds
+  FundingRound.all.select {|round| round.venture_capitalist == self}
+end
+
+def portfolio
+  funding_rounds.map {|round| round.startup}
+end
+
+def biggest_investment
+  total = 0
+  big_round = 0
+  funding_rounds.each do |round|
+    if round.investment > total
+      big_round = round.investment
+      total = round.investment
+    end
+  end
+  return big_round
+end
+
+def invested(domain)
+  domain_array = []
+  funding_rounds.each do |round|
+    if round.startup.domain == domain
+      domain_array << round.investment
+    end
+  end
+  domain_array.inject(0) {|sum,x| sum + x}
+end
+
+def self.all
+  @@all
+end
+
+def self.tres_commas_club
+  @@all.select do |capitalist|
+    capitalist.total_worth > 1000000000
+  end
+end
+
 end
--- tools/console.rb
+++ tools/console.rb
@@ -3,9 +3,24 @@ require_relative '../config/environment.rb'
 def reload
   load 'config/environment.rb'
 end
-# Insert code here to run before hitting the binding.pry
-# This is a convenient place to define variables and/or set up new object instances,
-# so they will be available to test and play around with in your console
+
+paypal = Startup.new("Paypal", "Elon Musk", "finance")
+uber = Startup.new("Uber", "Tom Fields", "transportation")
+Startup.find_by_founder("Tom Fields")
+timmy = VentureCapitalist.new("Tim James", 382322928)
+bob = VentureCapitalist.new("Bob Gatz", 2000000000)
+jim = VentureCapitalist.new("Jim Halpert", 30000000)
+invest = FundingRound.new(paypal, bob, "Angel", 5000000)
+new_round = uber.sign_contract(bob,"2nd round", 34225.0)
+new_round2 = uber.sign_contract(jim, "2nd round", 353646.0)
+new_round3 = bob.offer_contract(paypal, "angel", 4325.2)
+uber.num_funding_rounds
+uber.total_funds
+uber.investors
+uber.big_investors
+timmy.funding_rounds
+bob.biggest_investment
+bob.invested("finance")
 
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
